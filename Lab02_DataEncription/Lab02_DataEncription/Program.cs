87bae3212d29b0f870e7601b6e06b2bd60bc6519
SC_Jackie <jackiesalazar20@gmail.com>
2017-10-06 13:02:14 -0600

    RSA project Generate keys

---
+++ Lab02_DataEncription/Lab02_DataEncription/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Lab02_DataEncription
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo f;
+
+            string filePath= "", entry= "", type= "";
+            bool exit = true;
+            while (exit)
+            {
+                RSA rsa = new RSA();
+                Console.WriteLine("Write your user to generate key:");
+                Console.SetCursorPosition(32, Console.CursorTop - 1);
+                var user =  Console.ReadLine();
+                rsa.GenerateKeys();
+                Console.WriteLine("your private key is:" + rsa.j.ToString());
+               // rsa.Encryption();
+                Console.WriteLine("c:/encryption/:");
+                Console.SetCursorPosition(15, Console.CursorTop - 1);
+                entry = Console.ReadLine();
+                f = new DirectoryInfo(entry);
+                if (!Validation(entry, ref filePath, ref type))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("¡Error! Asegúrese de haber escrito correctamente los comandos del progama. Verifique que exista el archivo.");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    //Here goes RSA
+                   // exit = true;
+                }
+
+                
+            }
+            Console.ReadLine();
+           /* byte[] bytes;
+            using (var file = new FileStream("C:\\Users\\jsala\\Pictures\\emilyo.jpeg", FileMode.Open))
+            {
+                using (var binaryFile = new BinaryReader(file, Encoding.ASCII))
+                {
+                    var mensaje = binaryFile.ReadBytes((int)file.Length);
+                    // Instanciamos el algorimo asimétrico RSA
+
+                    bytes = mensaje;
+                }
+            }
+            using (var file = new FileStream("C:\\Users\\jsala\\Pictures\\emilyo.txt", FileMode.Append))
+            {
+                using (var binaryFile = new BinaryWriter(file, Encoding.ASCII))
+                {
+                   
+                    RSACryptoServiceProvider primerRSA = new RSACryptoServiceProvider();
+                    // Establecemos la longitud de la clave que queremos usar
+                    primerRSA.KeySize = 2048;
+                   
+                    bytes = primerRSA.Encrypt(bytes, true);
+                    for (int i = 0; i < bytes.Length; i++)
+                    {
+                        binaryFile.Write(bytes[i]);
+                    }
+
+                }
+            }*/
+            
+           
+          
+        }
+        //Validation
+        static bool Validation(string entry, ref string filePath, ref string type)
+        {
+            string[] current = entry.Split(' ');
+            if (current[0].ToLower() == "exit")
+            {
+                type = "exit";
+                return true;
+            }
+            else
+            {
+                if (!(current.Length > 2))
+                {
+                    if (!(current[0] == "-c0" || current[0] == "-d" || current[0] == "-c1"))
+                        return false;
+                    type = current[0];
+                    string[] path = current[1].Split('"');
+                    if (!(path.Length > 3))
+                    {
+                        if (!(path[0] == "-f" && File.Exists(path[1])))
+                            return false;
+                        filePath = path[1];
+                    }
+                    else
+                        return false;
+                    return true;
+
+                }
+                else
+                    return false;
+            }
+
+        }
+    }
+}
---
+++ Lab02_DataEncription/Lab02_DataEncription/RSA.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+
+namespace Lab02_DataEncription
+{
+    class RSA
+    {
+        /*ALgoritmo:
+        1. Generar las llaves privadas y públicas son dos números primos
+        2.  Cifrar el mensaje P^e = E ( mod n ) P es el mensaje en texto plano,n y e son la clave pública,E es el mensaje cifrado*/
+
+        // private BigInteger p { get; set; }
+        //public BigInteger q { get; set; }
+        private int p;
+        private int q;
+
+        private int n;  //llave pública y privada
+        /// <summary>
+        /// Public key
+        /// </summary>
+        public int e { get; set; }// Lave pública
+        /// <summary>
+        /// Private key
+        /// </summary>
+        public int j { get; set; } //llave privada
+        private string filePaht;
+       
+        public RSA()
+        {
+           
+        }
+        private void GeneratePrimeNumber()
+        {
+            Random r = new Random();
+            var n = r.Next(0, 59);
+           
+            p = PolinomialToGeneratePrimeNumber(n);
+            do
+            {
+                n = r.Next(0, 59);
+                q = PolinomialToGeneratePrimeNumber(n);
+            } while (q.CompareTo(p) ==0);
+            
+        }
+        private int PolinomialToGeneratePrimeNumber(int n)
+        {
+            //Polinomio de J. Brox que generan un número primo. El valor de n va entre cero y cincuenta nueve.
+            return 6 * (n ^ 2) - 342 * n + 4903;
+        }
+        private int GenerateValueN()
+        {
+            return p * q;
+        } 
+        private int PhiEulier()
+        {
+            return (p - 1) * (q - 1);
+        }
+        private int CoprimeNumber(int n)
+        {
+            int e = 1;
+            do
+                e++;
+            while ((e.CompareTo(n) != -1) || ModBetweenTwoNumbers(e, n) != 0);
+
+            return e;
+        }
+        private int ModBetweenTwoNumbers(int e, int n)
+        {
+            return n % e;
+        }
+
+        private int GenerateValueJ(int n, int j)
+        {
+            j = n;
+            do
+            {
+                j++;
+            } while (j % n == 1);
+            return j;
+        }
+        /// <summary>
+        /// Method to generate private and public key
+        /// </summary>
+        public void GenerateKeys()
+        {
+            GeneratePrimeNumber();
+            n = GenerateValueN();
+            var  z = PhiEulier();
+            e = CoprimeNumber(n);
+            j = GenerateValueJ(n,j);
+               
+        }
+
+        public byte[] Encryption(byte[] plainText)
+        {
+            // 2.Cifrar el mensaje P ^ e = E(mod n) P es el mensaje en texto plano,n y e son la clave pública,E es el mensaje cifrado
+            //y = x^e mod n  y = E(x) be the encryption function where x is an integer and y is            the encrypted form of x
+            
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                var a = (plainText[i] ^ e)% n;
+                
+
+            }
+        }
+
+    
+    }
+}
